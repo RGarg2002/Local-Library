67e0d750067210ed6bf3a172cc26bde3e544fca4
RGarg2002 <royalgarg36@gmail.com>
2024-11-17 16:30:45 +0530

    added book deletion page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -152,12 +152,21 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+  const bookId = req.params.id;
+  const [book, bookInstances] = await Promise.all([Book.findById(bookId).exec(), BookInstance.find({book: bookId})]);
+
+  res.render("book_delete", {
+    title: "Delete book",
+    book: book,
+    bookinstances: bookInstances
+  });
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  const bookId = req.body.bookid;
+  await Book.findByIdAndDelete(bookId);
+  res.redirect("/catalog/books");
 });
 
 // Display book update form on GET.
